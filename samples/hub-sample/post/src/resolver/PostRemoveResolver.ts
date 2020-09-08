39c1e5f185c1b3af4da236f0edb39909cf1bb214
Umed Khudoiberdiev <pleerock.me@gmail.com>
2020-09-08 20:59:05 +0500

    removed hub sample

--- samples/hub-sample/post/src/resolver/PostRemoveResolver.ts
+++
@@ -1,9 +0,0 @@
-import { resolver } from "@microframework/core"
-import { PostApp } from "../app"
-
-/**
- * "postRemove" mutation resolver.
- */
-export const PostRemoveResolver = resolver(PostApp, "postRemove", () => {
-  return {}
-})
--- samples/hub-sample/post/src/resolver/PostsResolver.ts
+++
@@ -1,9 +0,0 @@
-import { resolver } from "@microframework/core"
-import { PostApp } from "../app"
-
-/**
- * "posts" query resolver.
- */
-export const PostsResolver = resolver(PostApp, "posts", ({ filter }) => {
-  return []
-})
