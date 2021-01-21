a52355da75f2d12964365a92e797b20ef0c67a92
Dmitry Zotov <dmzt08@gmail.com>
2021-01-21 13:30:30 +0500

    renamed test;

--- tests/src/parser/parse-models/declaration-with-arrays/declaration-with-array-app.test.ts
+++ tests/src/parser/parse-models/declaration-with-arrays/declaration-with-array-app.test.ts
@@ -1,14 +1,15 @@
 import { parse } from "@microframework/parser"
 
-describe("parse arrays > array app", () => {
+describe("parse models > declaration with array", () => {
   test("model defined with arrays and declaration", () => {
-    const result = parse(__dirname + "/array-app.ts")
+    const result = parse(__dirname + "/declaration-with-array-app.ts")
     const categoryModel = result.models.find(
       (model) => model.typeName === "Category",
     )
     const posts = categoryModel!.properties.find(
       (property) => property.propertyPath === "Category.posts",
     )
+
     expect(posts!.kind).toBe("reference")
     expect(posts!.array).toBeTruthy()
     expect(posts!.properties.length).toEqual(0)
