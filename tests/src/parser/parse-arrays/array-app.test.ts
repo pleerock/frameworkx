9820f7694e005eb317c75061a6c33bc61feb8a14
Dmitry Zotov <dmzt08@gmail.com>
2021-01-21 13:18:59 +0500

    added test for arrays;

--- tests/src/parser/parse-arrays/array-app.test.ts
+++ tests/src/parser/parse-arrays/array-app.test.ts
@@ -1,8 +1,16 @@
 import { parse } from "@microframework/parser"
 
 describe("parse arrays > array app", () => {
-  test("model defined with arrays", () => {
+  test("model defined with arrays and declaration", () => {
     const result = parse(__dirname + "/array-app.ts")
-    console.log(JSON.stringify(result, undefined, 2))
+    const categoryModel = result.models.find(
+      (model) => model.typeName === "Category",
+    )
+    const posts = categoryModel!.properties.find(
+      (property) => property.propertyPath === "Category.posts",
+    )
+    expect(posts!.kind).toBe("reference")
+    expect(posts!.array).toBeTruthy()
+    expect(posts!.properties.length).toEqual(0)
   })
 })
