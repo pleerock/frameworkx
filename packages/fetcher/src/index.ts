bc065aa128b420f74bc88df761da7658feb96e32
Umed Khudoiberdiev <pleerock.me@gmail.com>
2021-02-04 20:37:28 +0500

    fixed BigInt error on not supporting it platforms (e.g. react native)

--- packages/fetcher/src/index.ts
+++ packages/fetcher/src/index.ts
@@ -5,9 +5,13 @@ if (!globalThis.fetch) {
 
 // unfortunately we have to patch it this way,
 // to make JSON.stringify to work for big int
-if (!(BigInt.prototype as any)["toJSON"]) {
-  ;(BigInt.prototype as any)["toJSON"] =
-    (BigInt.prototype as any)["toJSON"] ||
+if (
+  globalThis.BigInt &&
+  globalThis.BigInt.prototype &&
+  !(globalThis.BigInt.prototype as any)["toJSON"]
+) {
+  ;(globalThis.BigInt.prototype as any)["toJSON"] =
+    (globalThis.BigInt.prototype as any)["toJSON"] ||
     function (this: bigint) {
       return this.toString()
     }
